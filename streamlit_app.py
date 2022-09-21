c822d1807d4eb619c7b349bd37fd57fa12808606
Andreas Schmieg <114088592+andreasschmieg@users.noreply.github.com>
2022-09-21 11:01:50 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,13 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 # This is silly
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.header('Yucky Shit')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
+streamlit.header('YUMMM!!!')
+streamlit.text('Cheeseburger')
+streamlit.text('Double Cheeseburger')
+streamlit.text('Triple Cheeseburger')
+streamlit.text('Fat with Grease in Oil')

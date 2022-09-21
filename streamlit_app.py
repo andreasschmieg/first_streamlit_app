4a9dbce37d1a0d7129486460da31f7303f83d681
Andreas Schmieg <114088592+andreasschmieg@users.noreply.github.com>
2022-09-21 11:03:15 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,8 +7,13 @@ streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
+
 streamlit.header('YUMMM!!!')
 streamlit.text('Cheeseburger')
 streamlit.text('Double Cheeseburger')
 streamlit.text('Triple Cheeseburger')
 streamlit.text('Fat with Grease in Oil')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+streamlit.header('Build Your Own Fat Burger with Extra Cheese')

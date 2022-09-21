e9125423affa8ff6ab413bf3d2b60f42561f5ff2
Andreas Schmieg <114088592+andreasschmieg@users.noreply.github.com>
2022-09-21 11:06:42 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,6 @@
-import streamlit
-import pandas
-
-streamlit.title('My Parents New Healthy Diner')
 # This is silly
+import streamlit
+streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Yucky Shit')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
@@ -18,3 +16,8 @@ streamlit.text('Fat with Grease in Oil')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 streamlit.header('Build Your Own Fat Burger with Extra Cheese')
+
+# More serious
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
+import pandas

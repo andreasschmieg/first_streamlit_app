a6f5c0ec03c80b2af54fb1e74308d8d8012b5ea1
Andreas Schmieg <114088592+andreasschmieg@users.noreply.github.com>
2022-09-21 11:08:01 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,17 +7,9 @@ streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 
-streamlit.header('YUMMM!!!')
-streamlit.text('Cheeseburger')
-streamlit.text('Double Cheeseburger')
-streamlit.text('Triple Cheeseburger')
-streamlit.text('Fat with Grease in Oil')
-
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-streamlit.header('Build Your Own Fat Burger with Extra Cheese')
-
 # More serious
+import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)
-import pandas
